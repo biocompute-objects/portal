d91f77ceb369144fc2c40535761c113650584f44
Chris Armstrong <carmstrong1gw@github.com>
2021-05-05 14:24:40 -0400

    Added run server flag.

--- userdb/portalusers/core/rdb.sh
+++ userdb/portalusers/core/rdb.sh
@@ -12,5 +12,10 @@ rm db.sqlite3
 python3 manage.py makemigrations
 python3 manage.py migrate
 
-python3 manage.py runserver 8080
+if [[ $1 == '-r' ]]
+then
+
+	python3 manage.py runserver 8080
+
+fi
 
