b7182fec6a06c98ebcc9f1238aeb0ed5c82fea9b
John Torcivia <syntheticgio@aol.com>
2021-12-26 14:33:04 -0500

    color can't be set by hex here

--- src/views/builder/BuilderView/ColorCoded/ErrorDomain.js
+++ src/views/builder/BuilderView/ColorCoded/ErrorDomain.js
@@ -45,7 +45,7 @@ export default function ErrorDomain({ items, cF }) {
           <StyledCell colSpan="5">
             <Button
               variant="contained"
-              color="D5D8DC"
+              // color="D5D8DC"
               fullWidth
               onClick={() => window.open('https://docs.biocomputeobject.org/error-domain/')}
             >
