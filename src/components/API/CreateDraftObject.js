dada7dab0da46c86d755d73cddbefb36c6293907
Hadley King <hadley_king@gwu.edu>
2022-05-26 15:22:06 -0400

    Fix redirect on object creation Changes to be committed: 	modified:   src/components/API/CreateDraftObject.js

--- src/components/API/CreateDraftObject.js
+++ src/components/API/CreateDraftObject.js
@@ -4,7 +4,6 @@
 draft id */
 
 export default function CreateDraftObject(saveDraftTo, contents, prefix) {
-
   const objectContents = contents;
   console.log('objectContents', contents);
 
@@ -25,25 +24,21 @@ export default function CreateDraftObject(saveDraftTo, contents, prefix) {
       'Content-type': 'application/json; charset=UTF-8'
     }
   })
-    .then((response) => {
+    .then(async (response) => {
       if (!response.ok) {
         throw new Error(response.status);
       }
       if (response.status === 207) {
-        return response.json()
-          .then((data) => {
-            throw new Error(data[0].message);
-          });
-      };
+        const data = await response.json();
+        throw new Error(data[0].message);
+      }
       if (response.status === 200) {
-        return response.json()
-          .then((data) => {
-            console.log('data', data);
-            const objectId = data.object_id;
-            alert(`Create Draft Success! Save the following object ID to access later  ${data.object_id}`);
-            const processed = objectId.replace('://', '/');
-            window.location.href = `${window.location}/${processed}`;
-          });
+        const data = await response.json();
+        console.log('data', data);
+        const objectId = data[0].object_id;
+        alert(`Create Draft Success! Save the following object ID to access later  ${objectId}`);
+        const processed = objectId.replace('://', '/');
+        window.location.href = `${window.location}/${processed}`;
       }
     })
     .catch((error) => {
